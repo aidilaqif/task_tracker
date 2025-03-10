cf1988b98abfc8088936d70d86d44162149a0924
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2025-03-10 14:42:56 +0800

    Update home_page.dart

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -11,7 +11,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
   final ApiService apiService = ApiService();
   bool isLoading = true;
   String userName = "User";
@@ -32,18 +31,6 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  // void _incrementCounter() {
-  //   setState(() {
-  // This call to setState tells the Flutter framework that something has
-  // changed in this State, which causes it to rerun the build method below
-  // so that the display can reflect the updated values. If we changed
-  // _counter without calling setState(), then the build method would not be
-  // called again, and so nothing would appear to happen.
-  // _counter++;
-  //   });
-  // }
-
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
