ecfd9660d43bd52cbba26b68d983762149bf533b
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2025-04-11 14:34:24 +0800

    Update team.php

--- ci4_web_api/app/Views/team.php
+++ ci4_web_api/app/Views/team.php
@@ -4,5 +4,12 @@
 <div class="tasks-container">
     <h2>Team</h2>
     <p>This is the team section</p>
+    <table>
+        <tr>
+            <td>Team Name</td>
+            <td>Description</td>
+            <td>Members</td>
+        </tr>
+    </table>
 </div>
 <?= $this->endSection() ?>
